86f739503e96bea8ba2a468714b4210caace9fd7
DEnis <denis.demchuk96@mail.ru>
2015-07-14 23:52:55 +0300

    Add project

---
+++ fib.js
@@ -0,0 +1,11 @@
+var n = 0; 
+    
+function fib(n) {
+
+ if (n==1 || n==2) {
+ 	return 1;
+ }
+ 	return  fib(n-1)+fib(n-2); 	
+ 
+}
+console.log(fib(8));
---
+++ functions.js
@@ -0,0 +1,5 @@
+function calcDeterminate(a) {
+   var a = 0;
+   a = matrix[0][0]*matrix[1][1]-matrix[1][0]*matrix[0][1];
+   return a;
+}
