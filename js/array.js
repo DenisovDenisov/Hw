1643aae8e97151747399e3b55bc9a342310d688b
av <av@fun-box.ru>
2015-07-15 14:26:46 +0300

    another commit

--- js/array.js
+++
@@ -1,5 +0,0 @@
-     var i = 0,
-       array = [1,2,3,4,5,6];
-       console.log(array.length)
-       array.reverse();
-       console.log(array);
--- js/functions.js
+++
@@ -1,5 +0,0 @@
-function calcDeterminate(a) {
-   var a = 0;
-   a = matrix[0][0]*matrix[1][1]-matrix[1][0]*matrix[0][1];
-   return a;
-}
